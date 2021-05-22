7b46ab9b46a22344884f9a5ea854bd2782a04102
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-22 20:04:45 +0200

    added construct for coin

---
+++ src/Coin.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './Coin.css'
+
+class Coin extends Component {
+    render(){
+        return <img src="https://tinyurl.com/react-coin-heads-jpg" alt="Coin head" />
+    }
+}
+
+export default Coin;
