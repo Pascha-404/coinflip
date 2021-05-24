0e864fc7bf8eaab5671d50eac6e694dcbcf12c83
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-24 23:13:02 +0200

    final logic for coinGame

--- src/CoinGame.js
+++ src/CoinGame.js
@@ -4,15 +4,34 @@ import Coin from './Coin'
 
 class CoinGame extends Component{
     constructor(props){
-        super()
+        super(props)
+        this.state = {coinFace: "heads", countFlips: 0, countHeads: 0, countTails: 0}
+        this.handleClick = this.handleClick.bind(this)
     }
+
+    flipCoin(e){
+        const faces = ["heads", "tail-dime"];
+        const randomFace = faces[Math.floor(Math.random() * faces.length)];
+        this.setState(curState => ({countFlips: curState.countFlips + 1}))
+        if(randomFace === "heads"){
+        this.setState(curState => ({countHeads: curState.countHeads + 1}))
+        } else {
+            this.setState(curState => ({countTails: curState.countTails + 1}))
+        };
+        this.setState(curState => ({coinFace: randomFace}))
+    }
+
+    handleClick(e){
+        this.flipCoin()
+    }
+
     render(){
-        return(
+        return (
             <section className="CoinGame">
                 <h1>Lets flip the coin!</h1>
-                <Coin />
-                <button>Flip it</button>
-                <p>Out of X times, you had X heads and x tails</p>
+                <Coin face={this.state.coinFace}/>
+                <button className="CoinGame-btn" onClick={this.handleClick}>Flip it</button>
+                <p>Out of {this.state.countFlips} times, you had {this.state.countHeads} heads and {this.state.countTails} tails</p>
             </section>
         )
     }
